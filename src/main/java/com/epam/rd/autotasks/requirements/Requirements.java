f0b963c3a2b669affecff6ddfd9d9e7a6a4676f8
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-02-05 19:33:05 +0530

    done

--- src/main/java/com/epam/rd/autotasks/requirements/Requirements.java
+++ src/main/java/com/epam/rd/autotasks/requirements/Requirements.java
@@ -1,33 +1,58 @@
 package com.epam.rd.autotasks.requirements;
 
 public class Requirements {
-
-    public static void requireNonNull(Object obj) {
-
+    public static <T> T requireNonNull(T obj) {
+        if (obj == null) {
+            throw new NullPointerException();
+        }
+        return obj;
     }
 
-    public static void requireNonNull(Object obj, String message) {
-
+    public static <T> T requireNonNull(T obj, String message) {
+        if (obj == null) {
+            throw new NullPointerException(message);
+        }
+        return obj;
     }
 
-    public static void checkArgument(boolean value) {
+    public static void checkArgument(boolean expression) {
+        if (!expression) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public static void checkArgument(boolean value, String message) {
-
+    public static void checkArgument(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalArgumentException(message);
+        }
     }
 
-    public static void checkState(boolean value) {
-
+    public static void checkState(boolean expression) {
+        if (!expression) {
+            throw new IllegalStateException();
+        }
     }
 
-    public static void checkState(boolean value, String message) {
-
+    public static void checkState(boolean expression, String message) {
+        if (!expression) {
+            throw new IllegalStateException(message);
+        }
     }
 
     public static void checkIndex(int index, int size) {
-
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
     }
-
-
 }
+/*
+* The requireNonNull methods check if an object is null and throw
+*  a NullPointerException with an optional message if it is.
+
+The checkArgument and checkState methods check if a boolean expression is true or false,
+*  and throw an IllegalArgumentException or IllegalStateException with an optional message
+*  if the expression is false.
+
+The checkIndex method checks if an index is
+* within the bounds of an array or collection, and throws an IndexOutOfBoundsException
+*  if the index is out of bounds.*/
